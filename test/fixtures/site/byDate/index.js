ccdeed1820badd58f74976d9d5424ccdf89e53ac
Mark Smith <markjgsmith@gmail.com>
2023-10-29 11:13:23 +0700

    Migrate from msssg-plugin-postsbycalendar to msssg-plugin-posts

---
+++ test/fixtures/site/byDate/index.js
@@ -0,0 +1,14 @@
+---
+---
+<ul>
+  <li>
+    <a href="/byDate/notesByDateWithPrefix">
+      notesByDateWithPrefix
+    </a>
+  </li>
+  <li>
+    <a href="/byDate/notesByDateWithoutPrefix">
+      notesByDateWithoutPrefix
+    </a>
+  </li>
+</ul>
---
+++ test/fixtures/site/byItem/index.js
@@ -0,0 +1,14 @@
+---
+---
+<ul>
+  <li>
+    <a href="/byItem/notesByItemWithPrefix">
+      notesByItemWithPrefix
+    </a>
+  </li>
+  <li>
+    <a href="/byItem/notesByItemWithoutPrefix">
+      notesByItemWithoutPrefix
+    </a>
+  </li>
+</ul>
