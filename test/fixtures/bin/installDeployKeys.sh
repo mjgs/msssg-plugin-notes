20d5b648e981cf9788328180fe40deadae464f8b
Mark Smith <markjgsmith@gmail.com>
2023-12-02 18:57:51 +0700

    Add test data repo deploy key

--- test/fixtures/bin/installDeployKeys.sh
+++ test/fixtures/bin/installDeployKeys.sh
@@ -4,6 +4,7 @@ echo "Installing deploy keys..."
 
 BIN_DIR=$repo_dir/test/fixtures/bin
 
+$BIN_DIR/installDeployKey.sh "id_rsa_posts_dev" "$private_deploy_key_posts_dev"
 $BIN_DIR/installDeployKey.sh "id_rsa_scripts" "$private_deploy_key_scripts"
 $BIN_DIR/installDeployKey.sh "id_rsa_msssg" "$private_deploy_key_msssg"
 $BIN_DIR/installDeployKey.sh "id_rsa_ssg" "$private_deploy_key_ssg"
